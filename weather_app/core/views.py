b3ec1ef553130b7ece8c63102f623b4dfbfec7ce
Reenu Singh <singhreenu376@gmail.com>
2025-11-15 15:08:41 +0530

    Weather-APP

---
+++ weather_app/core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+def index(request):
+    weather_data = None
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        api_key = '72e05c931fcc6852a4e68acce01f9623'
+        url =f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+        response = requests.get(url) # requests for Fetching data from API
+
+        if response.status_code == 200:
+            response = response.json()
+            weather_data = {
+                'city':response['name'],
+                'temp': response['main']['temp'],
+                'humidity': response['main']['humidity'],
+                'wind_speed': response['wind']['speed'],
+                'description':response['weather'][0]['description'].title(),
+            }
+        else:
+            weather_data ={'error': 'City not found or API request failed.'}
+
+    return render(request, 'core/index.html',{'weather_data':weather_data})
